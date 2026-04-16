0e2348a80b19b7bd7f84d11ea9b49f17c041665b
Pasquale Sociale <pasquale.sociale@gmail.com>
2026-04-16 06:53:32 +0200

    feat: use ts_ls instead of vtsls

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -3,24 +3,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              tsserver = {
-                maxTsServerMemory = 4096,
-              },
-              implementationsCodeLens = { enabled = false },
-              referencesCodeLens = { enabled = false },
-            },
-            vtsls = {
-              autoUseWorkspaceTsdk = true,
-            },
-          },
-        },
-        eslint = {
-          settings = {
-            run = "onSave",
-          },
+        ts_ls = {
+          enabled = true,
         },
       },
     },
