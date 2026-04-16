217c734e63fe00d3f113105b9c15c3b7b30d142c
Pasquale Sociale <pasquale.sociale@gmail.com>
2026-04-16 06:57:20 +0200

    feat: add command to open file tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,4 +10,12 @@ return {
       },
     },
   },
+  keys = {
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer (buffers)" },
+    {
+      "<leader>E",
+      "<cmd>Neotree toggle filesystem<cr>",
+      desc = "Explorer (files)",
+    },
+  },
 }
