49ac544ec0f198807f0306923f42b880047887fa
Pasquale Sociale <pasquale.sociale@gmail.com>
2026-05-14 22:36:24 +0200

    fix: lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,10 +2,51 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = {
+        enabled = false,
+      },
       servers = {
-        ts_ls = {
-          enabled = true,
+        tsgo = {
+          filetypes = {
+            "typescript",
+            "typescriptreact",
+            "javascript",
+            "javascriptreact",
+          },
         },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tsgo",
+        "eslint_d",
+        "css-lsp",
+        "gopls",
+        "lua-language-server",
+        "prettier",
+        "prisma-language-server",
+        "shfmt",
+        "stylua",
+        "tailwindcss-language-server",
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        go = { "gofmt" },
       },
     },
   },
