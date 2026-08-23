0bfaa96081636bf23d3b595d511b2da4c90bef78
Pasquale Sociale <pasquale.sociale@gmail.com>
2026-08-23 22:08:51 +0200

    feat: ctrl l clears the terminal

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,16 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- ctrl+l clears the terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function(ev)
+    vim.keymap.set({ "n", "t" }, "<C-l>", function()
+      vim.fn.chansend(vim.bo[ev.buf].channel, "\x0c")
+      vim.schedule(function()
+        local sb = vim.bo[ev.buf].scrollback
+        vim.bo[ev.buf].scrollback = 1
+        vim.bo[ev.buf].scrollback = sb
+      end)
+    end, { buffer = ev.buf, desc = "clear terminal" })
+  end,
+})
